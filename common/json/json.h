73380f74d6fa40022b6d97e17f0c8edd3df6f489
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-06 23:59:13 +0100

    [enh] New type system

--- common/json/json.h
+++ common/json/json.h
@@ -9,22 +9,26 @@
 #define CLONE_METHOD(type) Value * clone(void) const {return new type(value());}
 
 namespace JSON {
+    enum Type {
+        Integer_t = 0x01,
+        Float_t   = 0x02,
+        String_t  = 0x04,
+        List_t    = 0x08,
+        Dict_t    = 0x10,
+    };
+
     class Value {
         public:
             virtual Value * clone(void) const = 0;
             virtual std::string dumps(void) const = 0;
-            virtual bool isSequence(void) const {return false;}
-            virtual bool isLitteral(void) const {return false;}
-            virtual bool isNumber(void)   const {return false;}
-            virtual bool isString(void)   const {return false;}
+            virtual Type type(void) const = 0;
+            bool isNumber() const {return (type()&(Integer_t|Float_t)) != 0;}
+            bool isAtom() const {return (type()&(Integer_t|Float_t|String_t)) != 0;}
+            bool isSequence() const {return (type()&(Dict_t|List_t)) != 0;}
     };
-    
-    class Litteral : public Value {
-        virtual bool isLitteral(void) const {return true;}
-    };
-    
+
     template <typename T>
-    class Number : public Litteral {
+    class Number : public Value {
         private:
             T _value;
         public:
@@ -32,14 +36,13 @@ namespace JSON {
             T const & value(void) const {return _value;}
             double doubleVal(void) const {return (double) _value;}
             long int intVal(void) const {return (long int) _value;}
-            bool isInteger(void) const {return intVal() == value();}
-            virtual bool isNumber(void) const {return true;}
     };
     
     class Integer : public Number<long int> {
         public:
             using Number<long int>::Number;
-            Value * clone(void) const {return new Integer(value());}
+            Type type(void) const {return Integer_t;}
+            CLONE_METHOD(Integer)
             std::string dumps() const {
                 char buffer[21];
                 sprintf(buffer, "%ld", value());
@@ -50,7 +53,8 @@ namespace JSON {
     class Float : public Number<double> {
         public:
             using Number<double>::Number;
-            Value * clone(void) const {return new Float(value());}
+            Type type(void) const {return Float_t;}
+            CLONE_METHOD(Float)
             std::string dumps() const {
                 char buffer[21];
                 snprintf(buffer, 21, "%lf", value());
@@ -58,29 +62,23 @@ namespace JSON {
             }
     };
     
-    class String : public Litteral {
+    class String : public Value {
         private:
             std::string _content;
         public:
             String(std::string str) : _content(str){};
+            Type type(void) const {return String_t;}
             CLONE_METHOD(String)
-            std::string dumps(void) const {
-                return "\"" + _content + "\"";
-            };
-            std::string const & value(void) const {return _content;};
-            virtual bool isString(void) const {return true;};
+            std::string dumps(void) const {return "\"" + _content + "\"";}
+            std::string const & value(void) const {return _content;}
     };
 
-    class Sequence : public Value {
-        public:
-            virtual bool isSequence(void) const {return true;}
-    };
-
-    class List : public Sequence {
+    class List : public Value {
         private:
             std::vector<Value*> _content;
         public:
             List() : _content() {};
+            Type type(void) const {return List_t;}
             Value * clone(void) const {return NULL;}
             std::string dumps(void) const {return "Hello";}
             const Value * operator[](size_t index){return _content[index];}
--- common/json/test.cpp
+++ common/json/test.cpp
@@ -1,36 +1,95 @@
 #include "json.h"
 #include "lighttest.h"
 
+void iThrow(void)
+{
+	throw "Hello, I'm an exception :)S";
+}
+
+TEST(lighttest)
+	ASSERT(true);
+	ASSERT(not false);
+	ASSERT_THROWS(const char *, iThrow());
+ENDTEST()
+
 TEST(json_int)
 	JSON::Integer i(3);
 	ASSERT(i.value() == 3);
 	ASSERT(i.dumps() == "3");
+	ASSERT(i.isAtom());
 	ASSERT(i.isNumber());
-	ASSERT(i.isInteger());
+	ASSERT(i.type() == JSON::Integer_t);
 ENDTEST()
 
 TEST(json_float)
-	JSON::Float i(3.14);
-	ASSERT(i.value() == 3.14);
-	ASSERT(i.dumps() == "3.140000");
-	ASSERT(i.isNumber());
-	ASSERT(! i.isInteger());
+	JSON::Float f(3.14);
+	ASSERT(f.value() == 3.14);
+	ASSERT(f.dumps() == "3.140000");
+	ASSERT(f.isAtom());
+	ASSERT(f.isNumber());
+	ASSERT(f.type() == JSON::Float_t);
 ENDTEST()
 
 TEST(json_str)
 	JSON::String s("Je suis un canape qui chante");
 	ASSERT(s.value() == "Je suis un canape qui chante");
 	ASSERT(s.dumps() == "\"Je suis un canape qui chante\"");
-	ASSERT(s.isString());
+	ASSERT(s.isAtom());
 	ASSERT(! s.isNumber());
+	ASSERT(s.type() == JSON::String_t);
+ENDTEST()
+
+TEST(json_list)
+	JSON::List l;
+	ASSERT(l.len() == 0);
+ENDTEST()
+
+TEST(json_list_append)
+	JSON::List l;
+	for (int i=0; i<10; i++)
+		l.append(JSON::Integer(i));
+	ASSERT(l.len() == 10);
+	JSON::Integer *cur;
+	for (int i=0; i<10; i++){
+		cur = (JSON::Integer *) l[i];
+		ASSERT(cur->value() == i);
+	}
+ENDTEST()
+
+TEST(json_list_mixed)
+	JSON::List l;
+	for (int i=0; i<10; i++){
+		if (i%2)
+			l.append(JSON::Integer(3));
+		else
+			l.append(JSON::Float(12.45));
+	}
+	ASSERT(l.len() == 10);
+	for (int i=0; i<10; i++){
+		ASSERT(l[i]->isNumber());
+		switch (l[i]->type()){
+			case JSON::Integer_t:
+				ASSERT(i%2);
+				ASSERT(((JSON::Integer*) l[i])->value() == 3);
+				break;
+			case JSON::Float_t:
+				ASSERT(i%2 == 0);
+				ASSERT(((JSON::Float*) l[i])->value() == 12.45);
+				break;
+			default: break;
+		}
+	}
 ENDTEST()
 
 int main(int argc, const char **argv)
 {
 	TestFunc testSuite[] = {
+		ADDTEST(lighttest),
 		ADDTEST(json_int),
 		ADDTEST(json_float),
-		ADDTEST(json_str)
+		ADDTEST(json_str),
+		ADDTEST(json_list),
+		ADDTEST(json_list_append)
 	};
 
 	return RUN(testSuite);
