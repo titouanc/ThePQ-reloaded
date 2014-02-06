9552a8efa028948daddcc7067601f2021be0a7b7
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-02-06 16:00:05 +0000

    [add] network->makefile
    Change-Id: I15846603d04c35d71b1924ada2629b39b6b38e7f

---
+++ network/test/maintest.cpp
@@ -0,0 +1,11 @@
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+int main( int argc, char **argv)
+{
+  CppUnit::TextUi::TestRunner runner;
+  CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+  runner.addTest(registry.makeTest());
+  bool hasFailed = runner.run("", false);
+  return (int) ! hasFailed;
+}
