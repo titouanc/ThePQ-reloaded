ecffeb4d15b79cac5469fab67d0f53a8a35c34ee
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-07 12:49:22 +0100

    [add] sorry, didn't add Register.cpp, Register.hpp and Constants.hpp to the repo.

---
+++ client/Register.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "Register.hpp"
+
+using namespace std;
+
+bool userWantsToRegister(){
+	bool done = false;
+	bool res = false;
+	while (!done){
+		cout << "Do you want to register? (y/n)" << endl;
+		string wantsToRegister;
+		cin >> wantsToRegister;
+		if (wantsToRegister == "y" || wantsToRegister == "Y") {
+			res = true;
+			done = true;
+		}
+		else if (wantsToRegister == "n" || wantsToRegister == "N") {
+			res = false;
+			done = true;
+		}
+		else
+			cout << "Please enter a valid response." << endl;
+	}
+	return res;
+}
+
+string askForNewPassword(){
+
+}
---
+++ client/Register.hpp
@@ -0,0 +1,6 @@
+#include <string>
+
+using namespace std;
+
+bool userWantsToRegister();
+string askForNewPassword();
