554fa569d460093430f59607c2f66fae81ea192c
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-07 11:49:58 +0100

    [add] Splash screen

--- client/Messages.cpp
+++ client/Messages.cpp
@@ -4,32 +4,25 @@
 using namespace std;
 
 void printLaunchingMessage(){
-	cout << "      .-." << endl;
-	cout << "      | |" << endl;
-	cout << "      |=|" << endl;
-	cout << "      |=|" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |       Welcome to the" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |          QUIDDITCH" << endl;
-	cout << "      | |             MANAGER" << endl;
-	cout << "      | |            2014!" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |" << endl;
-	cout << "      | |" << endl;
-	cout << "      |=|" << endl;
-	cout << "      |=|" << endl;
-	cout << "      |_|" << endl;
-	cout << "    .=/I\\=." << endl;
-	cout << "   ////V\\\\\\\\" << endl;
-	cout << "   |#######|" << endl;
-	cout << "   |||||||||" << endl;
-	cout << "   |||||||||" << endl;
-	cout << "   |||||||||" << endl;
-	cout << "   |||||||||" << endl;
-	cout << "   |||||||||" << endl;
+	cout << "           __       _   " << endl;
+	cout << "           //_ _   /_/_ _                                |" << endl;
+	cout << "          // //_' /  //_/                              - * -" << endl;
+	cout << "      ____       _     _     _     _ _ _       _         |" << endl;
+	cout << "     /___ \\_   _(_) __| | __| | __| (_) |_ ___| |__  " << endl;
+	cout << "    //  / / | | | |/ _` |/ _` |/ _` | | __/ __| '_ \\ " << endl;
+	cout << "   / \\_/ /| |_| | | (_| | (_| | (_| | | || (__| | | |" << endl;
+	cout << "   \\___,_\\ \\__,_|_|\\__,_|\\__,_|\\__,_|_|\\__\\___|_| |_|" << endl;
+	cout << "        =======||\\" << endl;
+	cout << "    ===========|| \\___________________________  ___            |" << endl;
+	cout << "  =============||  |__________________________==___|>        - * -" << endl;
+	cout << "    ===========|| /                                            |" << endl;
+	cout << "        =======||/     /\\/\\   __ _ _ __   __ _  __ _  ___ _ __  " << endl;
+	cout << "                      /    \\ / _` | '_ \\ / _` |/ _` |/ _ \\ '__|" << endl;
+	cout << "          |          / /\\/\\ \\ (_| | | | | (_| | (_| |  __/ |  " << endl;
+	cout << "        - * -        \\/    \\/\\__,_|_| |_|\\__,_|\\__, |\\___|_| " << endl;
+	cout << "          |                   _  _              |___/       " << endl;
+	cout << "                               )/ \\/||_|_ " << endl;
+	cout << "                              /_\\_/ |  |  " << endl;
+	cout << "" << endl;
+
 }
