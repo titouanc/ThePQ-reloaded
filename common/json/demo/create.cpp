26aef3781d107eab780f2378859f4638c8c5330e
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-09 15:40:34 +0100

    [add] JSON demos

---
+++ common/json/demo/create.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <json.h>
+
+using namespace std;
+
+int main(int argc, const char **argv)
+{
+    JSON::Dict manager;
+    manager.set("name", "Titouan");
+    manager.set("level", 15);
+    manager.set("fame", 17.2);
+
+    manager.set("players_id", JSON::List());
+    LIST(manager.get("players_id")).append(13);
+    LIST(manager.get("players_id")).append(17);
+
+    cout << "Output direct: " << manager << endl;
+    
+    string const & repr = manager.dumps();
+    cout << "Output string: " << repr << endl;
+
+    return 0;
+}
