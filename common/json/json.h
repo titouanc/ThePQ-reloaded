8290d92d615c31c40efa121b3e0ef3e5296dba90
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-09 04:56:29 +0100

    [ref] Split header

--- common/json/json.h
+++ common/json/json.h
@@ -60,31 +60,7 @@ namespace JSON {
             bool isSequence() const {return (type()&(Dict_t|List_t)) != 0;}
     };
 
-    template <typename T>
-    class Number : public Value {
-        private:
-            T _value;
-            virtual void _writeTo(std::ostream & out) const;
-        public:
-            Number(T initial);
-            ~Number();
-            T const & value(void) const;
-            double doubleVal(void) const;
-            long int intVal(void) const;
-            Value *clone(void) const;
-    };
-
-    class Integer : public Number<long int> {
-        public:
-            using Number<long int>::Number;
-            Type type(void) const;
-    };
-    
-    class Float : public Number<double> {
-        public:
-            using Number<double>::Number;
-            Type type(void) const;
-    };
+    #include "number.h"
     
     class String : public Value {
         private:
@@ -146,47 +122,6 @@ namespace JSON {
     };
 
     Value *parse(const char *str, char **eptr=NULL);
-
-    /* ===== Number<T> ===== */
-    template <typename T>
-    Number<T>::Number(T initial) : _value(initial)
-    {}
-
-    template <typename T>
-    Number<T>::~Number()
-    {}
-
-    template <typename T>
-    T const & Number<T>::value(void) const 
-    {
-        return _value;
-    }
-
-    template <typename T>
-    double Number<T>::doubleVal(void) const 
-    {
-        return (double) _value;
-    }
-
-    template <typename T>
-    long int Number<T>::intVal(void) const 
-    {
-        return (long int) _value;
-    }
-
-    template <typename T>
-    Value * Number<T>::clone(void) const
-    {
-        if (ISINT(this))
-            return new Integer(_value);
-        return new Float(_value);
-    }
-
-    template <typename T>
-    void Number<T>::_writeTo(std::ostream & out) const
-    {
-        out << _value;
-    }
 }
 
 #endif
