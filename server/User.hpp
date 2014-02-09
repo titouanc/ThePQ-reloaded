50095902cc0daa4f3e0959f7af1fbf3bd00076c9
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-09 17:47:07 +0100

    [fix] corrected Socket type to TcpSocket

--- server/User.hpp
+++ server/User.hpp
@@ -6,13 +6,13 @@ using namespace std;
 class User{
 private:
 	string _username;
-	Socket _socket;
+	TcpSocket _socket;
 public:
 	string getUsername() { return _username; }
 	void setUsername(string username){ _username = username; };
 
-	Socket getSocket() { return _socket; }
-	void setSocket(Socket socket) { _socket = socket; }
+	TcpSocket getSocket() { return _socket; }
+	void setSocket(TcpSocket socket) { _socket = socket; }
 
 	string getUserPath() { return "users/"+getUsername(); }
 };
