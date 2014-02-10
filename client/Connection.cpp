d259a5ee25fab5c85a16eee768fa7dba2a024a62
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-02-10 14:17:12 +0000

    [rmv] network in Connection.cpp
    Change-Id: Iedd0d6ca7d1b17b35834a4c2b87e52b4702c8f16

--- client/Connection.cpp
+++ client/Connection.cpp
@@ -5,7 +5,6 @@ using namespace std;
 Connection::Connection()
 {
 	_isLogged = false;
-	_socket.connect("127.0.0.1", 6666);
 }
 
 bool Connection::isLogged()
@@ -15,17 +14,7 @@ bool Connection::isLogged()
 
 void Connection::loginUser(string username, string passwd)
 {
-	JSON::Dict data;
-	data.set("username", username);
-	data.set("passwd", passwd);
-	net::Message *msg = new net::Message(net::Message::Request::LOGIN, 
-											net::Message::Method::REQUEST, 
-											&data);
-	cout << "Client sending: " << msg->dumps() << endl;
-	_socket.send(msg);
-	delete msg;
-	//~ _socket.recv((JSON::Value**)&msg);
-	//~ cout << "Client receiving: " << msg->dumps() << endl;
+	
 }
 
 void Connection::registerUser(string username, string passwd)
--- client/Connection.hpp
+++ client/Connection.hpp
@@ -19,8 +19,6 @@ public:
 private:
 	bool _isLogged;
 	
-	net::TcpSocket _socket;
-	
 };
 
 #endif
