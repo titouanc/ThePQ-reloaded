65f0e0167a6e40cc2c21973c8603812d28f660d4
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-10 17:34:08 +0100

    [add] a few tests, all working.

--- client/InstallationTest.cpp
+++ client/InstallationTest.cpp
@@ -1,5 +1,6 @@
 #include "../common/lighttest/lighttest.h"
 #include "Installation.hpp"
+#include <cmath>
 
 TEST(default_constructor)
 	Installation i;
@@ -23,8 +24,33 @@ TEST(setters)
 	ASSERT(i.getLevel() == 5);
 ENDTEST()
 
-TEST(upgrade)
-	Installation i(0, 100);
+TEST(upgrade_installation)
+	Installation inst(0, 100);
+	for (int i = 0; i < 5; ++i){
+		inst.upgrade();
+		ASSERT(inst.getLevel() == i+1);
+		ASSERT(inst.getCurrentValue() == 100*pow(2, i));
+	}
+ENDTEST()
+
+TEST(downgrade_installation)
+	Installation inst(0, 100);
+	for (int i = 0; i < 5; ++i){
+		inst.upgrade();
+	}
+	for (int i = 0; i < 4; ++i){
+		inst.downgrade();
+		cout << "level : " << inst.getLevel() << endl;
+		cout << "getCurrentValue : " << inst.getCurrentValue() << endl;
+		ASSERT(inst.getLevel() == 5-i-1);
+		ASSERT(inst.getCurrentValue() == 100*pow(2, inst.getLevel()-1));
+
+	}
+	ASSERT(inst.getValueAtLevel(inst.getLevel()) == 100);
+	inst.downgrade();
+	ASSERT(inst.getLevel() == 0);
+	ASSERT(inst.getCurrentValue() == 0);
+	ASSERT(inst.getValueAtLevel(0) == 0);
 ENDTEST()
 
 
@@ -32,7 +58,9 @@ int main(int argc, const char **argv){
 	TestFunc tests[] = {
 		ADDTEST(default_constructor),
 		ADDTEST(parameters_constructor_and_getters),
-		ADDTEST(setters)
+		ADDTEST(setters), 
+		ADDTEST(upgrade_installation),
+		ADDTEST(downgrade_installation)
 	};
 	return RUN(tests);
 }
