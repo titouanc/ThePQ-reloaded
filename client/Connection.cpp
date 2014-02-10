6101d6373431404a3e29773934664818a25f6e90
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-02-10 10:48:07 +0000

    [enh] client -> Connection
    Change-Id: I1698f033416fa399418181f6367cd04bdc638f15

--- client/Connection.cpp
+++ client/Connection.cpp
@@ -1,6 +1,5 @@
 #include "Connection.hpp"
-#include "Exception.hpp"
-
+#include <iostream>
 using namespace std;
 
 Connection::Connection()
@@ -16,7 +15,18 @@ bool Connection::isLogged()
 
 void Connection::loginUser(string username, string passwd)
 {
-	throw UserNotFoundException();
+	JSON::Dict* data = new JSON::Dict();
+	data->set("username", JSON::String(username));
+	data->set("passwd", JSON::String(passwd));
+	net::Message *msg = new net::Message(net::Message::Request::LOGIN, 
+											net::Message::Method::REQUEST, 
+											data);
+	_socket.send(msg);
+	cout << "Client sending: " << msg->dumps() << endl;
+	delete msg;
+	delete data;
+	_socket.recv((JSON::Value**)&msg);
+	cout << "Client receiving: " << msg->dumps() << endl;
 }
 
 void Connection::registerUser(string username, string passwd)
--- client/Connection.hpp
+++ client/Connection.hpp
@@ -3,6 +3,7 @@
 
 #include "net.hpp"
 #include "json.h"
+#include "Exception.hpp"
 #include <string>
 
 class Connection
