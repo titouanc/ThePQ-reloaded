f6a955e268e56817e67a9b094f2163243f1fa4a9
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-10 11:30:34 +0100

    [fix] change currentValue of the installation in upgrade and downgrade.

--- client/Installation.cpp
+++ client/Installation.cpp
@@ -4,21 +4,27 @@ int Installation::getMaintenanceCost(){
 }
 
 int Installation::getUpgradeCost(){
+	// TODO test this with getDowngradeRefunds!
 	float multiplicator = 1 + (getLevel()*0.1);
 	return getCurrentValue() * multiplicator;
 }
 
 int Installation::getDowngradeRefunds(){
+	// TODO test this with getUpgradeCost!
 	float divider = 1 + ((getLevel()-1)*0.1);
 	return getCurrentValue() / divider;
 }
 
 void Installation::upgrade(){
-	if (getLevel() < getMaxLevel())
+	if (getLevel() < getMaxLevel()){
+		setCurrentValue(getUpgradeCost());
 		setLevel( getLevel() + 1 );
+	}
 }
 
 void Installation::downgrade(){
-	if (getLevel() > 0)
+	if (getLevel() > 0){
+		setCurrentValue(getDowngradeRefunds());
 		setLevel( getLevel() - 1 );
+	}
 }
