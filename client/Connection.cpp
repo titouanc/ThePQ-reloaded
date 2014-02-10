0bdeda27587b207f140da7b4453e649448ae7c37
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-02-10 13:54:38 +0000

    [fix] network Message.cpp
    Change-Id: I05f5d0fa687f50a5138549b2b5bb13abb13c965a

--- client/Connection.cpp
+++ client/Connection.cpp
@@ -16,16 +16,16 @@ bool Connection::isLogged()
 void Connection::loginUser(string username, string passwd)
 {
 	JSON::Dict data;
-	data.set("username", JSON::String(username));
-	data.set("passwd", JSON::String(passwd));
+	data.set("username", username);
+	data.set("passwd", passwd);
 	net::Message *msg = new net::Message(net::Message::Request::LOGIN, 
 											net::Message::Method::REQUEST, 
 											&data);
 	cout << "Client sending: " << msg->dumps() << endl;
 	_socket.send(msg);
 	delete msg;
-	_socket.recv((JSON::Value**)&msg);
-	cout << "Client receiving: " << msg->dumps() << endl;
+	//~ _socket.recv((JSON::Value**)&msg);
+	//~ cout << "Client receiving: " << msg->dumps() << endl;
 }
 
 void Connection::registerUser(string username, string passwd)
--- common/network/Message.cpp
+++ common/network/Message.cpp
@@ -2,7 +2,7 @@
 		
 net::Message::Message(net::Message::Request request, net::Message::Method method, JSON::Value *data) : JSON::Dict()
 {
-	set("Request", JSON::Integer(request));
-	set("Method", JSON::Integer(method));
+	set("Request", request);
+	set("Method", method);
 	set("Data", *data);
 }
