9ceb558950637c903bb2cb2e8746ffc7dbc248ce
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-12 21:44:43 +0100

    [fix] normally all register+connection messages should be treated correctly.

--- server/Server.cpp
+++ server/Server.cpp
@@ -20,17 +20,23 @@ void Server::treatMessage(const Message &message){
 	cout << "\t" << *(message.data) << endl;
 	if (ISDICT(message.data)){
 		JSON::Dict const &received = DICT(message.data);
-		if (received.hasKey("type") && ISSTR(received.get("type")) 
-			&& (received.hasKey("data") && ISDICT(received.get("data")))){
-			string messageType = STR(received.get("type")).value();
-			if (messageType == "CO_L"){
-				logUserIn(DICT(received.get("data")), message.peer_id);
+		if (received.hasKey("type") && ISSTR(received.get("type"))) {
+			if (received.hasKey("data") && ISDICT(received.get("data"))){
+				// data is a DICT
+				string messageType = STR(received.get("type")).value();
+				if (messageType == "CO_L"){
+					logUserIn(DICT(received.get("data")), message.peer_id);
+				}
+				else if (messageType == "CO_R") {
+					registerUser(DICT(received.get("data")), message.peer_id);
+				}
 			}
-			else if (messageType == "CO_R") {
-				registerUser(DICT(received.get("data")), message.peer_id);
-			}
-			else if (messageType == "CO_U") {
-				checkIfUserExists(DICT(received.get("data")), message.peer_id);
+			else if (received.hasKey("data") && ISSTR(received.get("data"))){
+				// data is a STR
+				string messageType = STR(received.get("type")).value();
+				if (messageType == "CO_U") {
+					checkIfUserExists(STR(received.get("data")).value(), message.peer_id);
+				}
 			}
 		}
 	}
@@ -97,25 +103,22 @@ void Server::logUserIn(const JSON::Dict &credentials, int peer_id){
 	}
 }
 
-void Server::checkIfUserExists(const JSON::Dict &credentials, int peer_id){
-	if (credentials.hasKey("CO_U") && ISSTR(credentials.get("CO_U"))
-		&& credentials.hasKey("CO_P") && ISSTR(credentials.get("CO_P" ))){
-		User *user = User::load(STR(credentials.get("CO_U")).value());
-		if (user != NULL){
-			// user found
-			JSON::Dict * statusDict = new JSON::Dict();
-			statusDict->set("type", "CO_S");
-			statusDict->set("data", "U_REG");
-			Message status(peer_id, statusDict);
-			_outbox.push(status);
-		}
-		else {
-			// user not found
-			JSON::Dict * statusDict = new JSON::Dict();
-			statusDict->set("type", "CO_S");
-			statusDict->set("data", "U_N_F");
-			Message status(peer_id, statusDict);
-			_outbox.push(status);
-		}
+void Server::checkIfUserExists(string username, int peer_id){
+	User *user = User::load(username);
+	if (user != NULL){
+		// user found
+		JSON::Dict * statusDict = new JSON::Dict();
+		statusDict->set("type", "CO_S");
+		statusDict->set("data", "U_REG");
+		Message status(peer_id, statusDict);
+		_outbox.push(status);
+	}
+	else {
+		// user not found
+		JSON::Dict * statusDict = new JSON::Dict();
+		statusDict->set("type", "CO_S");
+		statusDict->set("data", "U_N_F");
+		Message status(peer_id, statusDict);
+		_outbox.push(status);
 	}
 }
