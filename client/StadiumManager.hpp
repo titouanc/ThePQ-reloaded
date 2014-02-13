86385383a588ce436f1b7388f396983a4d990c85
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-13 11:55:52 +0100

    [add] ifndef, _installation attribute, changed makefile to include StadiumManager in objects.

--- client/StadiumManager.hpp
+++ client/StadiumManager.hpp
@@ -1,5 +1,19 @@
+#ifndef __STADIUM_MANAGER_HPP
+#define __STADIUM_MANAGER_HPP 
+
+#include <string>
+#include <map>
+#include "Installation.hpp"
+
+using namespace std;
+
 class StadiumManager {
+public:
 	StadiumManager();
 
 	void printInstallationsList();
+private:
+	map<string, Installation> _installations;
 };
+
+#endif
