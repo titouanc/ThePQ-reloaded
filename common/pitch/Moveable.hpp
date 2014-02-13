b3cec441b28aa6ada63b9961d798a774849ca7ac
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-13 14:09:09 +0100

    [enh] Moveable serialisation/deserialization

--- common/pitch/Moveable.hpp
+++ common/pitch/Moveable.hpp
@@ -12,6 +12,13 @@ public:
 	Moveable(): _name("NoName"), _uniqueID(0), _speed(0), _position(Position(0,0)){}
 	Moveable(string name, unsigned int id, float speed, Position &position): 
 				_name(name), _uniqueID(id), _speed(speed), _position(position){}
+	Moveable(JSON::Dict const & json){
+		if (ISSTR(json.get("name"))) _name = STR(json.get("name")).value();
+		if (ISINT(json.get("ID"))) _uniqueID = INT(json.get("ID")).value();
+		if (ISLIST(json.get("position"))) _position = Position(LIST(json.get("position")));
+		if (ISFLOAT(json.get("speed"))) _speed = FLOAT(json.get("speed")).value();
+		else if (ISINT(json.get("speed"))) _speed = INT(json.get("speed")).value();
+	}
 	~Moveable(){}
 
 	// GETTERS + SETTERS
@@ -27,10 +34,19 @@ public:
 	unsigned int getID() const { return _uniqueID; }
 	void setID(unsigned int newID) { _uniqueID = newID; }
 
+	JSON::Dict toJson(void) const {
+		JSON::Dict res;
+		res.set("speed", _speed);
+		res.set("position", _position.toJson());
+		res.set("ID", _uniqueID);
+		res.set("name", _name);
+		return res;
+	}
+
 private:
 	string _name;
 	unsigned int _uniqueID;
-	float _speed;
+	double _speed;
 	Position _position;
 };
 
