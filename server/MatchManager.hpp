ce049cc6b1c31efded19006dadaf3931720a042b
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-14 01:15:52 +0100

    [ref] Follow other changes

--- server/MatchManager.hpp
+++ server/MatchManager.hpp
@@ -1,11 +1,12 @@
 #ifndef DEFINE_MATCHMANAGER_HEADER
 #define DEFINE_MATCHMANAGER_HEADER 
 
-#include <cassert>
 #include <queue>
 #include <pitch/Moveable.hpp>
 #include <pitch/Displacement.hpp>
+#include <pitch/Pitch.hpp>
 #include <cstring>
+#include "User.hpp"
 
 class Player : public Moveable {
 	using Moveable::Moveable;
@@ -22,40 +23,26 @@ struct Stroke {
 
 struct Squad {
 	Player *players[7];
-	std::string managerName;
-};
-
-class Pitch {
-	private:
-		size_t _width, _height;
-		Moveable **_matrix;
-		size_t _index(int x, int y) const {
-			x += _width/2;
-			y += _height/2;
-			assert(0<=x && x<(int)_width);
-			assert(0<=y && y<(int)_height);
-			return x*_width + y;
+	Squad(){memset(players, 0, 7*sizeof(Player*));}
+	Squad(JSON::List const & json){
+		if (json.len() != 7)
+			throw JSON::KeyError("A Squad contains exactly 7 players");
+		for (size_t i=0; i<7; i++){
+			if (! ISDICT(json[i]))
+				throw JSON::TypeError("Expecting JSON::Dict for player");
+			players[i] = new Player(DICT(json[i]));
 		}
-	public:
-		Pitch(size_t width, size_t height) : _width(width), _height(height){
-			_matrix = new Moveable*[width*height];
-			memset(_matrix, 0, width*height*sizeof(Moveable*));
-		}
-		~Pitch(){delete[] _matrix;}
-
-		Moveable * getAt(int x, int y) const {
-			return _matrix[_index(x, y)];
-		}
-		void setAt(int x, int y, Moveable *moveable){
-			Position initial = moveable->getPosition();
-			Position dest(x, y);
-			size_t fromPos = _index(initial.x(), initial.y());
-			if (! (initial == dest) && _matrix[fromPos] == moveable){
-				_matrix[fromPos] = NULL;
-				moveable->setPosition(dest);
-			}
-			_matrix[_index(x, y)] = moveable;
-		}
+	}
+	~Squad(){
+		for (size_t i=0; i<7; i++)
+			delete players[i];
+	}
+	JSON::List toJson(){
+		JSON::List res;
+		for (int i=0; i<7; i++)
+			res.append(players[i]->toJson());
+		return res;
+	}
 };
 
 class MatchManager {
@@ -66,11 +53,32 @@ class MatchManager {
 		Pitch _pitch;
 	public:
 		MatchManager() : _strokes(), _pitch(100, 36) {
-			memset(_squads, 0, 2*sizeof(Squad));
-			memset(_balls, 0, 4*sizeof(Ball*));
+			for (int i=0; i<4; i++)
+				_balls[i] = new Ball();
+			cout << _pitch;
 		}
 		~MatchManager();
-		void onCollision(Stroke & s1, Stroke & s2, Position &conflict){
+
+		/*!
+		 * @meth void MatchManager::addStroke(Stroke const & stroke)
+		 * @brief Add a stroke to the current turn
+		 */
+		void addStroke(Stroke const & stroke);
+
+		/*!
+		 * @meth void MatchManager::playStrokes(void)
+		 * @brief Play all strokes in this turn
+		 */
+		void playStrokes(void);
+
+		/*!
+		 * @meth void MatchManager::onCollision(Stroke & s1, Stroke & s2, Position &conflict)
+		 * @brief Callback called when a collision occurs.
+		 * @param s1 Stroke object for the 1st player on the conflicting cell
+		 * @param s2 Stroke object for the 2nd player on the conflicting cell
+		 * @param conflict The conflicting cell position
+		 */
+		virtual void onCollision(Stroke & s1, Stroke & s2, Position &conflict){
 			std::cout << "Collision " << s1.moveable.getName() << " & "
 			          << s2.moveable.getName() << " => " << conflict.toJson();
 		}
