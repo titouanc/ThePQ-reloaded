92b7e540c9f7b463ee84350fd105b9a6c94e86c7
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-14 02:07:08 +0100

    [ref] Clean code

--- server/Server.cpp
+++ server/Server.cpp
@@ -40,38 +40,30 @@ void Server::run(){
 void Server::treatMessage(const Message &message){
 	if (ISDICT(message.data)){
 		JSON::Dict const &received = DICT(message.data);
-		if (received.hasKey("type") && ISSTR(received.get("type"))) {
+		if (ISSTR(received.get("type"))) {
 			string messageType = STR(received.get("type")).value();
-			if (received.hasKey("data") && ISDICT(received.get("data"))){
-				// data is a DICT
-				if (messageType == net::MSG::LOGIN_QUERY){
+			if (messageType == "DISCONNECT"){
+				map<int, User*>::iterator it = _users.find(message.peer_id);
+				if (it != _users.end()){
+					delete it->second;
+					_users.erase(it);
+				}
+			} else if (ISDICT(received.get("data"))){
+				if (messageType == MSG::LOGIN_QUERY)
 					logUserIn(DICT(received.get("data")), message.peer_id);
-				}
-				else if (messageType == net::MSG::REGISTER_QUERY) {
+				else if (messageType == MSG::REGISTER_QUERY) 
 					registerUser(DICT(received.get("data")), message.peer_id);
-				}
-			}
-			else if (received.hasKey("data") && ISSTR(received.get("data"))){
+			} else if (ISSTR(received.get("data"))){
 				// data is a STR
-				if (messageType == net::MSG::USER_EXISTS_QUERY) {
-					checkIfUserExists(STR(received.get("data")).value(), message.peer_id);
-				}
-				else if (messageType == net::MSG::DATA_QUERY){
-					if(STR(received.get("data")).value() == net::MSG::INSTALLATIONS_LIST){
+				string const & query = STR(received.get("data"));
+				if (messageType == MSG::USER_EXISTS_QUERY) {
+					checkIfUserExists(query, message.peer_id);
+				} else if (messageType == MSG::DATA_QUERY){
+					if (query == net::MSG::INSTALLATIONS_LIST){
 						sendInstallationsList(message.peer_id);
 					}
-					else if(STR(received.get("data")).value() == net::MSG::CONNECTED_USERS_LIST){
+					else if(query == net::MSG::CONNECTED_USERS_LIST){
 						sendConnectedUsersList(message.peer_id);
-					}
-				}
-			}
-			else if (received.hasKey("data") && ISINT(received.get("data"))){
-				// data is an INT
-				if (messageType == "DISCONNECT"){
-					map<int, User*>::iterator it = _users.find(message.peer_id);
-					if (it != _users.end()){
-						delete it->second;
-						_users.erase(it);
 					}
 				}
 			}
