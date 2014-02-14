9114c0f439bec48b37cd2227b11080d6c4deb0ae
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-02-14 21:59:37 +0000

    [add] net::MSG constants for installations upgrade and downgrade
    Change-Id: I91848e001679317157843d2e608e09f08779d05f

--- common/network/TcpSocket.hpp
+++ common/network/TcpSocket.hpp
@@ -45,6 +45,8 @@ namespace net
 		static const std::string USER_REGISTERED = "U_R";
 		// data queries
 		static const std::string INSTALLATIONS_LIST = "DQ_I_L";
+		static const std::string INSTALLATION_UPGRADE = "DQ_I_U";
+		static const std::string INSTALLATION_DOWNGRADE = "DQ_I_D";
 		static const std::string CONNECTED_USERS_LIST = "DQ_CU_L";
 
 	}
