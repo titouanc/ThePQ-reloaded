ad6cd2372735607385ff8b947cc4876134245281
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-14 15:26:53 +0100

    [add] Client netconfig (should be used instead of static ip)

--- client/Client.hpp
+++ client/Client.hpp
@@ -9,7 +9,26 @@
 #include "Menu.hpp"
 #include "StadiumManager.hpp"
 #include "FriendlyGameManager.hpp"
+#include <Config.hpp>
 
+struct NetConfig : public Config {
+    std::string host;
+    unsigned short port;
+
+    NetConfig() : Config("netconfig.json"), host("127.0.0.1"), port(32123){}
+    void fromDict(JSON::Dict const & json){
+        if (ISSTR(json.get("host")))
+            host = STR(json.get("host")).value();
+        if (ISINT(json.get("port")))
+            port = INT(json.get("port"));
+    }
+    operator JSON::Dict() const{
+        JSON::Dict res;
+        res.set("host", host);
+        res.set("port", port);
+        return res;
+    }
+};
 
 class Client
 {
