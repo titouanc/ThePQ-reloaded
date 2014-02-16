94213c7f2f52e7676447d36f2bb16cf7e3a3ede5
Anthony Caccia <acaccia@ulb.ac.be>
2014-02-16 15:26:51 +0100

    [fix] seeker apt done
    Change-Id: Icd98e97bba2e9d6251c1938a343f3e8593eb2562

--- Skeleton/Seeker.cpp
+++ Skeleton/Seeker.cpp
@@ -1,8 +1,11 @@
 #include "Seeker.hpp"
 #include "Player.hpp"
 
-using namespace std;
-
 bool Seeker::isSeeker() const {
-    return True;
+    return true;
 }
+
+int Seeker::catchGS (){
+    return static_cast<int>( (float) precision * .50 + (float) velocity * .20 + (float) strength * .10 + (float) magic * .1 + (float) chance * .1 ) * 100 ;
+}
+
--- Skeleton/Seeker.hpp
+++ Skeleton/Seeker.hpp
@@ -1,6 +1,8 @@
 #ifndef Seeker_hpp
 #define Seeker_hpp
 
+#include "Player.hpp"
+
 class Seeker : public Player 
 {
 public:
