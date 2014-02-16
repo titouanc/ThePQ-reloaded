677fac039998073efe44477bc1b5675d5d0415f8
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-16 18:26:47 +0100

    [ref] Jersey

--- common/model/Jersey.hpp
+++ common/model/Jersey.hpp
@@ -2,21 +2,22 @@
 #define __JERSEY_HPP
 
 #include "Gear.hpp"
-#include "Player.hpp"
 
 class Jersey : Gear 
 {
 public:
-	int getStrengthBonus () const;
-	int getConstitutionBonus () const;
-	int getMagicBonus () const;
-	int getSpiritBonus () const;
+	Jersey():strengthBonus(0), constitutionBonus(0), magicBonus(0), spiritBonus(0){}
+
+	int getStrengthBonus () const 		{ return _strengthBonus; }
+	int getConstitutionBonus () const 	{ return _; }
+	int getMagicBonus () const 			{ return _magicBonus; }
+	int getSpiritBonus () const 		{ return _spiritBonus; }
 private:
     Jersey();
-	int strengthBonus;
-	int constitutionBonus;
-	int magicBonus;
-	int spiritBonus;
+	int _strengthBonus;
+	int _constitutionBonus;
+	int _magicBonus;
+	int _spiritBonus;
 };
 
 #endif
