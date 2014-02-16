98c398bb590ba66f8d11b74efa3c5afe19dc1e5e
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-16 17:35:26 +0100

    [ref] Moved Quaffle, GoldenSnitch and Bludger in Ball

--- common/model/Ball.hpp
+++ common/model/Ball.hpp
@@ -1,13 +1,29 @@
-#ifndef Ball_hpp
-#define Ball_hpp
+#ifndef __BALL_HPP
+#define __BALL_HPP
 
 #include "Moveable.hpp"
 
 class Ball : Moveable {
 public:
-	bool isQuaffle ();
-	bool isBludger ();
-	bool isGoldenSnitch ();
+	virtual bool isQuaffle () { return false; }
+	virtual bool isBludger () { return false; }
+	virtual bool isGoldenSnitch () { return false; }
 };
+
+class GoldenSnitch : Ball {
+public:
+	bool isGoldenSnitch () { return true; }
+};
+
+class Bludger : Ball {
+public:
+	bool isBludger () { return true; }
+};
+
+class Quaffle : Ball {
+public:
+	bool isQuaffle () { return true; }
+};
+
 
 #endif
