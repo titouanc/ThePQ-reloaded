0ba4fef8d6532cf7a09db0b22a5e7c2c7215a253
Jerome Hellinckx <jerhelli@ulb.ac.be>
2014-02-17 00:47:38 +0100

    [add] thread in server/Sale.hpp

--- server/Sale.hpp
+++ server/Sale.hpp
@@ -5,6 +5,8 @@
 #include <list>
 #include <queue>
 #include <ctime>
+#include <unistd.h>
+#include <pthread.h>
 #include <json/json.hpp>
 #include <network/TcpSocket.hpp>
 
@@ -13,22 +15,41 @@
 #define BIDRATIO 0.05 	//5%
 class Sale{
 private:
-	list<Team> _turnTeams;
+	vector<Team> _turnTeams;
 	queue<Team> _bidTeams;
 	int _bidValue;
-	int _nextBidValue;
 	float _bidRatio;
 	int _turn;
 	time_t _begin;
 	Player _player;
+	pthread_t thread;
+	static void * staticSaleStart(void * p);
+	void saleStart();
 public:
-	Sale(const JSON::Dict *json): {
-		LIST(json->get("turn_Teams"));
-	}
-	void run();
+	void start();
+	Sale(const JSON::Dict *json);
 	void save();
 	Sale load(std::string playerid);
 };
 
+Sale::Sale(const JSON::Dict *json): _bidValue(INT(json->get("bidValue").value())), _bidRatio(BIDRATIO), 
+	_turn(1), _begin(std::time(NULL), _player(json->get("player").value()){}
+
+void Sale::start(){
+	if(pthread_create(&thread, NULL, Sale::staticSaleStart, this) != 0){throw "Error occured when creating sale thread";
+}
+
+void * Sale::staticSaleStart(void * p){
+	((Sale *) p)->saleStart();
+	return NULL;
+}
+
+void Sale::saleStart(){
+	if(_turn == 1){std::sleep(FIRST_TURN);}	//In case you load an existing sale (server crash...)
+	while(_turnTeams.size() > 1){
+		std::sleep(TURN_TIME);
+		++_turn;
+	}
+}
 
 #endif
