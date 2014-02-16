6ca0e7f344a7c9129d4aa5803e3eb9124728925e
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-16 18:18:43 +0100

    [ref] Food

--- common/model/Food.hpp
+++ common/model/Food.hpp
@@ -1,15 +1,18 @@
 #ifndef __FOOD_HPP
 #define __FOOD_HPP
 
+#include <string>
+
 class Food 
 {
 public:
-    std::string getName () const ;
-	int getPrice () const ;
-	void setPrice (int newPrice);
+	Food(string name, int price): _name(name), _price(price){}
+    std::string getName () const 	{ return _name; }
+	int getPrice () const 			{ return _price; }
+	void setPrice (int newPrice)	{ _price = newPrice; }
 private:
-    std::string const name;
-	int price;
+    std::string const _name;
+	int _price;
 };
 
 #endif
