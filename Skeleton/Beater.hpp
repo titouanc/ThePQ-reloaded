6217b999c3ad366f01bb64d0ddeceda4abc035b0
Anthony Caccia <acaccia@ulb.ac.be>
2014-02-16 13:58:37 +0100

    [fix] specific att added
    Change-Id: Ia4dd50571a4edb32b4d3d4349477e74a705dd202

--- Skeleton/Beater.hpp
+++ Skeleton/Beater.hpp
@@ -4,14 +4,17 @@
 #include "Player.hpp"
 #include "Bat.hpp"
 
-class Beater : Player 
+class Beater : public Player 
 {
 public:
 	bool isBeater () const;
 	void equipBat (Bat aBat);
     void unequipBat ();
+    int collision ();
+    int anticollision ();
+    int shootBludger ();
 private:
-	Bat bat;
+	Bat * bat;
 };
 
 #endif
