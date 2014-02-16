f2a3eca5f04ca0e2ddd406e0ef3ba4ce977da58f
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-16 18:57:01 +0100

    [ref] put Bat, Broomstick and Jersey in Gear.hpp

--- common/model/Gear.hpp
+++ common/model/Gear.hpp
@@ -13,11 +13,58 @@ public:
 														_price(price) {}
     string getName () const 		{ return _name; }
     string getDescription () const	{ return _description; }
-	int getPrice () const;				{ return _price; }
+	int getPrice () const;			{ return _price; }
 private:
     string const _name;
     string const _description;
 	int const _price;
+};
+
+
+// BAT -------------------------------------------------------------------------
+class Bat : Gear 
+{
+public:
+    Bat(const int const strength, const int const precision) : 	_strengthBonus(strength),
+    															_precisionBonus(precision){}
+	int getStrengthBonus () const	{ return _strengthBonus; }
+	int getPrecisionBonus () const	{ return _precisionBonus; }
+private:
+	int const _strengthBonus;
+	int const _precisionBonus;
+};
+
+
+// BROOMSTICK ------------------------------------------------------------------
+class Broomstick : Gear 
+{
+public:
+    Broomstick(const int nCases, const int velocity):	_cases(nCases), 
+    													_velocityBonus(velocity){}
+	int getCases () const 			{ return _cases; }
+	int getVelocityBonus () const  	{ return _velocityBonus; }
+private:
+	const int _cases;
+	const int _velocityBonus;
+};
+
+
+// JERSEY ----------------------------------------------------------------------
+class Jersey : Gear 
+{
+public:
+	Jersey():strengthBonus(0), constitutionBonus(0), magicBonus(0), spiritBonus(0){}
+
+	int getStrengthBonus () const 		{ return _strengthBonus; }
+	int getConstitutionBonus () const 	{ return _; }
+	int getMagicBonus () const 			{ return _magicBonus; }
+	int getSpiritBonus () const 		{ return _spiritBonus; }
+private:
+    Jersey();
+	int _strengthBonus;
+	int _constitutionBonus;
+	int _magicBonus;
+	int _spiritBonus;
 };
 
 #endif
