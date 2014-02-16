eb035b314d5727e6fff88956c62b97c3888f590d
Anthony Caccia <acaccia@ulb.ac.be>
2014-02-16 15:31:11 +0100

    [fix] missing include in Team
    Change-Id: I0308e035ae7025140d25420f4cd2237bfc356c5f

--- Skeleton/Member.hpp
+++ Skeleton/Member.hpp
@@ -1,16 +1,19 @@
 #ifndef Member_hpp
 #define Member_hpp  
 
+#include "Team.hpp"
+
 class Member 
 {
 public:
-	string getName ();
+    Member();
+    std::string getName ();
 	int getSalary ();
 	int getPrice ();
 	Team getOwner ();
 	void setOwner (Team aTeam);
 private:
-	string name;
+    std::string name;
 	int salary;
 	int price;
 	Team owner;
