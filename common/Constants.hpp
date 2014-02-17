a6822ac31d3eee46268fb9e7dc97df71e58b9c53
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-17 14:02:29 +0100

    [ref] put net:MSG constants in common/Constants.hpp

--- common/Constants.hpp
+++ common/Constants.hpp
@@ -1,1 +1,34 @@
-#define HASH_KEY "OrnythorinqueSurTrotinetteInterlope"
+#ifndef __CONSTANTS_HPP
+#define __CONSTANTS_HPP
+
+namespace net
+{
+	// NETWORK MESSAGES --------------------------------------------------------
+	namespace MSG
+	{
+		// TYPE ----------------------------------------------
+		static const std::string LOGIN_QUERY = "Q_L";
+		static const std::string REGISTER_QUERY = "Q_R";
+		static const std::string USER_EXISTS_QUERY = "Q_U";
+		static const std::string CONNECTION_STATUS = "Q_S";
+		static const std::string DATA_QUERY = "Q_D";
+		static const std::string DATA_SEND = "S_D";
+		static const std::string INSTALLATION_UPGRADE = "AQ_I_U";
+		static const std::string INSTALLATION_DOWNGRADE = "AQ_I_D";
+		
+		// DATA ----------------------------------------------
+		// connection + register
+		static const std::string USERNAME = "D_U";
+		static const std::string PASSWORD = "D_P";
+		static const std::string USER_LOGIN = "U_L";
+		static const std::string PASSWORD_ERROR = "U_P";
+		static const std::string USER_NOT_FOUND = "U_NF";
+		static const std::string USER_EXISTS = "U_E";
+		static const std::string USER_REGISTERED = "U_R";
+		// data queries
+		static const std::string INSTALLATIONS_LIST = "DQ_I_L";
+		static const std::string CONNECTED_USERS_LIST = "DQ_CU_L";
+	}
+}
+
+#endif
