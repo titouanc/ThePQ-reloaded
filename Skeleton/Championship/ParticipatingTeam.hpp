7efbee9b7897cb1350368859a4f9c44220994a15
Mircea Mudura <mmudura@ulb.ac.be>
2014-02-18 00:47:43 +0100

    [enh] added comments for ParticipatingTeam.cpp/hpp [fix] added constructor && destructor for ParticipatingTeam

--- Skeleton/Championship/ParticipatingTeam.hpp
+++ Skeleton/Championship/ParticipatingTeam.hpp
@@ -4,12 +4,14 @@
 class ParticipatingTeam 
 {
 public:
+	ParticipatingTeam();
 	void addPoints (int toAdd);
 	int getPoints ();
 	void changeGoalAverage (int toChange);
 	int getGoalAverage ();
     std::string getName ();
 	int getLevel ();
+	~ParticipatingTeam();
 private:
 	int points;
 	int goalAverage;
