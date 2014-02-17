ef57e9ef07c6ab1dc82deea43c8fd13f101e4b6e
Mircea Mudura <mmudura@ulb.ac.be>
2014-02-18 00:01:16 +0100

    [enh]commented Championship.cpp/hpp [fix]added constructor for Championship [enh]commented TrainingGroup.cpp/hpp

--- Skeleton/Championship/Championship.hpp
+++ Skeleton/Championship/Championship.hpp
@@ -8,12 +8,14 @@
 class Championship 
 {
 public:
+	Championship();
 	ParticipatingTeam[] getTeams ();
 	void startChampionship ();
 	void addTeam (ParticipatingTeam team);
 	void deleteTeam (ParticipatingTeam team);
 	Schedule getSchedule (ParticipatingTeam team);
 	Ranking getRanking ();
+	~Championship();
 private:
 	ParticipatingTeam[] teams;
 	Ranking ranking;
