ac3b29673977ca17906d51b0d08b54853be75071
Florentin Hennecker <fhenneck@ulb.ac.be>
2014-02-17 12:01:43 +0100

    [enh] added commits number, total number of lines and COLORZ to delation.sh niark niark

--- delation.sh
+++ delation.sh
@@ -5,6 +5,14 @@ JEROME=0
 ANTHONY=0
 MIRCEA=0
 
+echo -e "\033[31mDISCLAIMER : \033[0mCes chiffres sont purement informatifs."
+echo "             Le nombre de lignes peut varier incroyablement en fonction de refactoring divers."
+echo "Commits : `git log | grep "Author:" | wc -l`"
+echo "Commits par personne : "
+git shortlog -sn
+
+echo "Calcul du nombre de lignes par personne..."
+
 for file in `find . -name *.[c,h]pp`; do
 	TITOUAN=$((TITOUAN + `git blame $file | grep -c "Titouan Christophe"`))
 	ANTOINE=$((ANTOINE + `git blame $file | grep -c "Antoine Carpentier"`))
@@ -20,3 +28,5 @@ echo "Florentin : $FLORENTIN"
 echo "Jerome : $JEROME"
 echo "Anthony : $ANTHONY"
 echo "Mircea : $MIRCEA"
+
+echo -e "\033[32mNombre de lignes total\033[0m : $((TITOUAN+ANTOINE+FLORENTIN+JEROME+ANTHONY+MIRCEA))"
