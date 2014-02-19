eb2a72216f7dd7d1d6934a5dfe2f8c2c6c09fb8c
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-02-19 19:02:13 +0000

    [add] Member ID
    Change-Id: I0ba1c5ff35fb9c29aade3ca6d5136fd26c2119cb

--- common/model/Member.hpp
+++ common/model/Member.hpp
@@ -11,14 +11,16 @@ class Member
 {
 public:
 	// TODO set random name in constructor-<<<<load first/last name from playerNames.txt
-    Member() : _name("Lasty Cotter"), _salary(5000), _price(25000){}
+    Member() : _memberID(-1), _name("Lasty Cotter"), _salary(5000), _price(25000){}
     Member(JSON::Dict const& json): Member() {
+		if (ISINT(json.get("memberID"))) _memberID = INT(json.get("memberID"));
     	if (ISSTR(json.get("name"))) _name = STR(json.get("name")).value();
     	if (ISINT(json.get("salary"))) _salary = INT(json.get("salary")).value();
     	if (ISINT(json.get("price"))) _price = INT(json.get("price")).value();
     }
     operator JSON::Dict(){
 		JSON::Dict res;
+		res.set("memberID", _memberID);
 		res.set("name", _name);
 		res.set("salary", _salary);
 		res.set("price", _price);
@@ -28,9 +30,12 @@ public:
     string getName () 	{ return _name; }
 	int getSalary () 	{ return _salary; }
 	int getPrice () 	{ return _price; }
+	int getMemberID()	{ return _memberID; }
 	//Team getOwner () 	{ return _owner; } // TODO
 	//void setOwner (Team aTeam) { _owner = aTeam; } // TODO
+	void setMemberID(int id) { _memberID = id; }
 protected:
+	int _memberID;
     string _name;
 	int _salary;
 	int _price;
