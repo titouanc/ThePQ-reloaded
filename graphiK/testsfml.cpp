3219f7148415e2662b082816753bb53503c360aa
Titouan Christophe <tichrist@ulb.ac.be>
2014-02-25 23:33:17 +0100

    [ref] Continue researchs on clic-to select on pitch

--- graphiK/testsfml.cpp
+++ graphiK/testsfml.cpp
@@ -17,7 +17,7 @@ using namespace std;
 class GraphicPitch {
 	private:
 		Pitch & _pitch;
-		unsigned int _size;
+		int _size;
 		sf::Texture _grass_texture, _sand_texture, _goal_texture;
 		sf::CircleShape _grass, _sand;
 		sf::RectangleShape _overlay;
@@ -49,14 +49,14 @@ class GraphicPitch {
 		int width() const {return _pitch.width()*_size/2;}
 		int height() const {return _pitch.height()*vOffset();}
 		void renderTo(sf::RenderTarget & dest){
-			unsigned int xwin=0, ywin=0;
+			int xwin=0, ywin=0;
 			for (int y=_pitch.ymin(); y<_pitch.ymax(); y++){
 				xwin = (abs(y)%2)*_size/2;
 				for (int x=_pitch.xmin(); x<_pitch.xmax(); x++){
 					if (! _pitch.isValid(x, y)) 
 						continue; /* Skip position that arent valid */
 					if (_pitch.inEllipsis(x, y)){
-						Position const & destpos = game2gui(x, y);
+						Position const & destpos = game2GUI(x, y);
 						if (_pitch.isInKeeperZone(x, y)){
 							_sand.setPosition(destpos.x(), destpos.y());
 							dest.draw(_sand);
@@ -73,35 +73,31 @@ class GraphicPitch {
 				}
 				ywin += vOffset();
 			}
-			drawOverlay(dest, game2gui(0, 0));
-			drawOverlay(dest, game2gui(_pitch.xmin(), 0));
-			drawOverlay(dest, game2gui(_pitch.xmax()-1, 0));
-			drawOverlay(dest, game2gui(0, _pitch.ymin()));
-			drawOverlay(dest, game2gui(0, _pitch.ymax()-1));
+			drawOverlay(dest, game2GUI(0, 0));
+			drawOverlay(dest, game2GUI(_pitch.xmin(), 0));
+			drawOverlay(dest, game2GUI(_pitch.xmax()-1, 0));
+			drawOverlay(dest, game2GUI(0, _pitch.ymin()));
+			drawOverlay(dest, game2GUI(0, _pitch.ymax()-1));
 		}
 
 		/* Translate a position in GUI coordinate system to game coordinates
 		   where the upper left corner of the pitch is at (left,top) in window */
-		Position gui2game(Position const & pos, int left=0, int top=0){
-			Position absolute(pos-Position(left, top));
-			int x = 2*((double)absolute.x())/(double)_size + _pitch.xmin();
-			int y = -((double)absolute.y()/vOffset() + _pitch.ymin());
-			if (! _pitch.isValid(x, y)){
-				cout << "DECREMENT X" << endl;
-				x--;
-			}
-			return Position(x, y);
+		Position GUI2game(Position const & pos){
+			int x = (2*pos.x() - (width()-_size)/2)/_size;
+			int y = (pos.y() - (height()-vOffset())/2)/vOffset();
+			return Position(round(x), round(y));
 		}
-		Position gui2game(int x, int y, int left=0, int top=0){
-			return gui2game(Position(x, y), left, top);
+		Position GUI2game(int x, int y, int left=0, int top=0){
+			return GUI2game(Position(x, y)) - Position(left, top);
 		}
-		Position game2gui(Position const & pos, int left=0, int top=0){
-			int y = (-pos.y()-_pitch.ymin())*vOffset();
-			int x = _size*(pos.x()-_pitch.xmin())/2;
-			return Position(x+left, y+top);
+		Position game2GUI(Position const & pos){
+			int x = (width()-_size)/2 + pos.x()*_size/2;
+			int y = (height()-vOffset())/2 - pos.y()*vOffset();
+			cout << pos.toJson() << " -> " << x << ", " << y << endl;
+			return Position(round(x), round(y));
 		}
-		Position game2gui(int x, int y, int left=0, int top=0){
-			return game2gui(Position(x, y), left, top);
+		Position game2GUI(int x, int y, int left=0, int top=0){
+			return game2GUI(Position(x, y)) + Position(left, top);
 		}
 
 		void drawOverlay(sf::RenderTarget & dest, int x, int y){
@@ -114,14 +110,14 @@ class GraphicPitch {
 
 		void onClick(sf::RenderTarget & dest, int x, int y){
 			Position arg(x, y);
-			Position const & clicked = game2gui(gui2game(arg));
+			Position const & clicked = game2GUI(GUI2game(arg));
 			Position const & delta = clicked-arg;
 
 			drawOverlay(dest, clicked);
 			if (abs(delta.x()) > _size || abs(delta.y()) > vOffset()){
 				JSON::Dict res = {
 					{"event", arg.toJson()},
-					{"gamepos", gui2game(x, y).toJson()},
+					{"gamepos", GUI2game(x, y).toJson()},
 					{"clicked", clicked.toJson()},
 					{"delta", delta.toJson()}
 				};
