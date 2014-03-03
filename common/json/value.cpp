68b22e8f70275321f3223545e4d04be7608a1075
Titouan Christophe <tichrist@ulb.ac.be>
2014-03-04 00:59:08 +0100

    [enh] JSON::load and JSON::Value::save now lock file

--- common/json/value.cpp
+++ common/json/value.cpp
@@ -5,6 +5,7 @@
 extern "C" {
 	#include <unistd.h>
 	#include <sys/types.h>
+	#include <sys/file.h>
 	#include <fcntl.h>
 	#include <errno.h>
 	#include <string.h>
@@ -26,26 +27,36 @@ void Value::save(const char *filename) const
 	int fd, r=0;
 
 	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0644);
-	if (fd < 0)
-	{
+	if (fd < 0){
 		std::string error = strerror(errno);
 		error += " in ";
 		error += filename;
 		throw IOError(error.c_str());
 	}
 
+	/* Acquire an exclusive lock on this file */
+	int lock = flock(fd, LOCK_EX);
+	if (lock != 0){
+		std::string error("Couldn't lock file ");
+		error += filename;
+		close(fd);
+		throw IOError(error.c_str());
+	}
+
 	std::string repr = this->dumps();
 	for (size_t i=0; i<repr.length(); i+=r){
 		r = write(fd, repr.c_str()+i, repr.length()-i);
-		if (r < 0)
-		{
+		if (r < 0){
 			std::string error = strerror(errno);
 			error += " in ";
 			error += filename;
+			flock(fd, LOCK_UN);
+			close(fd);
 			throw IOError(error.c_str());
 		}
 	}
 
+	flock(fd, LOCK_UN);
 	close(fd);
 }
 
