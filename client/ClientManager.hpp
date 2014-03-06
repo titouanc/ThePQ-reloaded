2065536de78cd991e59c45606ad1ed8bda1a233b
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-03-06 22:34:22 +0000

    [ref] ClientManager handles end of sale notification
    Change-Id: I63f5c478624b5e4a8c4a5f2db769f2c9093ca0b7

--- client/ClientManager.hpp
+++ client/ClientManager.hpp
@@ -3,6 +3,8 @@
 
 #include <network/ClientConnectionManager.hpp>
 #include "UserData.hpp"
+#include <sstream>
+#include <string>
 
 class ClientManager {
 	private:
@@ -27,9 +29,9 @@ class ClientManager {
 			{
 
 			}
-			else if (type == net::MSG::BID_ENDED)
+			else if (type == net::MSG::MARKET_MESSAGE)
 			{
-				_notifications.push("Bid ended!");
+				handleEndOfSaleNotification(data);
 			}
 		}
 
@@ -42,6 +44,31 @@ class ClientManager {
 	            treatMessage(messageType, dict.get("data")); /* virtuelle */
 	            delete msg;
 	        }
+		}
+
+		void handleEndOfSaleNotification(JSON::Value const * message){
+			std::stringstream res;
+			JSON::Dict const & json = DICT(message);
+			res << "\n\033[36mMessage : a sale has ended.\033[0m" << endl;
+			if(STR(json.get("type")).value()==net::MSG::END_OF_OWNED_SALE_RAPPORT){
+				if(STR(json.get(net::MSG::RAPPORT_SALE_STATUS)).value() == net::MSG::PLAYER_NOT_SOLD){
+					res << "Your player " << INT(json.get(net::MSG::PLAYER_ID)) << " has not been sold." << endl; 
+				}
+				else if(STR(json.get(net::MSG::RAPPORT_SALE_STATUS)).value() == net::MSG::PLAYER_SOLD){
+					res << "Your player " << INT(json.get(net::MSG::PLAYER_ID)) << " has been sold for " << INT(json.get(net::MSG::BID_VALUE)) 
+					<< " to " << STR(json.get(net::MSG::CURRENT_BIDDER)).value() << endl;
+				}
+			}
+			else if(STR(json.get("type")).value()==net::MSG::WON_SALE_RAPPORT){
+				std::string owner = STR(json.get(net::MSG::SALE_OWNER)).value();
+				res << "You bought player " << INT(json.get(net::MSG::PLAYER_ID)) << " for \33[32m" << INT(json.get(net::MSG::BID_VALUE)) << "\033[0m." <<endl;
+				if(owner==net::MSG::GENERATED_BY_MARKET)
+					res << "This player did not belong to any team. He was free. Like the wind."<<endl;
+				else
+					res << "This player comes from " << owner << "'s team." << endl;
+			}
+			res<<endl;
+			_notifications.push(res.str());
 		}
 
 	public:
