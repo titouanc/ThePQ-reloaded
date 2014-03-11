a121bc3cfad204bc50fa397dd71cdf2dccd6e0c5
Titouan Christophe <tichrist@ulb.ac.be>
2014-03-12 00:31:01 +0100

    [doc] Doxygen style ..Brandon Smith..

--- client/user/UserManager.hpp
+++ client/user/UserManager.hpp
@@ -9,38 +9,38 @@
 class UserManager : public ClientManager
 {
 protected:
-	/* attempt to login */
+	/*! attempt to login */
 	void loginUser(std::string username, std::string password);
-	/* notify the server to logout */
+	/*! notify the server to logout */
 	void logoutUser();
-	/* attempt to register */
+	/*! attempt to register */
 	void registerUser(std::string username, std::string password);
-	/* unused for the moment : check if a user exists TODO */
+	/*! unused for the moment : check if a user exists TODO */
 	void doesUserExist(std::string username);
-	/* choose a team name (on first login) */
+	/*! choose a team name (on first login) */
 	void chooseTeamName(std::string username, std::string teamname);
-	/* Treat user specific messages */
+	/*! Treat user specific messages */
 	virtual void treatMessage(std::string const & type, JSON::Value const * data);
 	
 	/* Hooks */
-	/* Triggered after successful login */
+	/*! Triggered after successful login */
 	virtual void onLoginOK(){}
 
-	/* Triggered after login failed.
+	/*! Triggered after login failed.
 	   @param err_message: an error text */
 	virtual void onLoginError(std::string const & err_message){}
 
-	/* Triggered when server ask a new team name */
+	/*! Triggered when server ask a new team name */
 	virtual void onAskTeamName(){}
 
-	/* Triggered after a successful register */
+	/*! Triggered after a successful register */
 	virtual void onRegisterUserOK() {}
-	/* Trigered when register fails */
+	/*! Trigered when register fails */
 	virtual void onRegisterUserError(std::string const & reason) {}
 
-	/* Triggered when new team name is accepted */
+	/*! Triggered when new team name is accepted */
 	virtual void onTeamNameOK() {}
-	/* Triggered when team name is rejected */
+	/*! Triggered when team name is rejected */
 	virtual void onTeamNameError(std::string const & reason) {}
 public:
 	using ClientManager::ClientManager;
