dbd6bed48109989c19405e00dc45283105b6eacb
Jerome Hellinckx <jerhelli@ulb.ac.be>
2014-03-13 01:03:33 +0100

    [add] current champ view when champ not started

--- client/championship/GraphicChampionshipManager.cpp
+++ client/championship/GraphicChampionshipManager.cpp
@@ -71,7 +71,77 @@ void GraphicChampionshipManager::seeCurrentChampionship(){
 	_canvas.clear();
 	usernameButton(user().username);
 	userBudgetButton(user().funds);
+	if(! user().joinedChamp.getName().empty()){
+		Championship & champ = user().joinedChamp;
+		TableView & champInfos = _canvas.addTableView();
+		champInfos.setPosition((window().getSize().x - 850) / 2, (2*MARGIN) + BUTTON_HEIGHT);
 
+		TableCell & header = champInfos.addTableCell(850, 47);
+		header.setBackgroundColor(BUTTON_BACKGROUND_COLOR);
+		Label & nameHeader = header.addLabel("Championship");
+		nameHeader.setPosition(15, 10);
+		nameHeader.setColor(BUTTON_TEXT_COLOR);
+		Label & statusHeader = header.addLabel("Status");
+		statusHeader.setPosition(255, 10);
+		statusHeader.setColor(BUTTON_TEXT_COLOR);
+		Label & cashHeader = header.addLabel("Cash prize");
+		cashHeader.setPosition(435, 10);
+		cashHeader.setColor(BUTTON_TEXT_COLOR);
+		Label & fameHeader = header.addLabel("Fame prize");
+		fameHeader.setPosition(655, 10);
+		fameHeader.setColor(BUTTON_TEXT_COLOR);
+
+		TableCell & champCell = champInfos.addTableCell(850,47);
+		champCell.setBackgroundColor(sf::Color(0x00, 0x00, 0x00, 0x77));
+		Label & name = champCell.addLabel(champ.getName());
+		name.setPosition(15, 10);
+		name.setColor(BUTTON_TEXT_COLOR);
+		std::stringstream status;
+		status <<  champ.getUsers().size() << "/" << champ.getNbOfUsers();
+		Label & statusLabel = champCell.addLabel(status.str());
+		statusLabel.setPosition(255, 10);
+		statusLabel.setColor(BUTTON_TEXT_COLOR);
+		Label & cashLabel = champCell.addLabel(champ.getCashPrize());
+		cashLabel.setPosition(435, 10);
+		cashLabel.setColor(GREEN_TEXT_COLOR);
+		Label & fameLabel = champCell.addLabel(champ.getFame());
+		fameLabel.setPosition(655, 10);
+		fameLabel.setColor(BLUE_TEXT_COLOR);
+
+		int cellWidth = 200;
+		TableView & users = _canvas.addTableView();
+		users.setPosition((window().getSize().x - cellWidth) / 2, (4*BUTTON_HEIGHT) + (3*MARGIN));
+		TableCell & header = users.addTableCell(cellWidth,47);
+		header.setBackgroundColor(BUTTON_BACKGROUND_COLOR);
+		Label & stringHeader = header.addLabel("Users");
+		stringHeader.setPosition((cellWidth - stringHeader.getWidth())/2,10);
+		stringHeader.setColor(BUTTON_TEXT_COLOR);
+		for(std::string & name : user().joinedChamp.getUsers()){
+			TableCell & userCell = users.addTableCell(cellWidth,47);
+			userCell.setBackgroundColor(sf::Color(0x00, 0x00, 0x00, 0x77));
+			Label & nameLabel = userCell.addLabel(name);
+			nameLabel.setPosition((cellWidth - nameLabel.getWidth())/2, 10);
+			nameLabel.setColor(BUTTON_TEXT_COLOR);
+		}
+		if(user().joinedChamp.isStarted()){
+			users.setPosition((window().getSize().x - cellWidth) / 4, (4*BUTTON_HEIGHT) + (3*MARGIN));
+			TableView & users = _canvas.addTableView();
+			users.setPosition((window().getSize().x - cellWidth) / 2, (4*BUTTON_HEIGHT) + (3*MARGIN));
+			TableCell & header = users.addTableCell(cellWidth,47);
+			header.setBackgroundColor(BUTTON_BACKGROUND_COLOR);
+			Label & stringHeader = header.addLabel("Users");
+			stringHeader.setPosition((cellWidth - stringHeader.getWidth())/2,10);
+			stringHeader.setColor(BUTTON_TEXT_COLOR);
+			for(std::string & name : user().joinedChamp.getUsers()){
+				TableCell & userCell = users.addTableCell(cellWidth,47);
+				userCell.setBackgroundColor(sf::Color(0x00, 0x00, 0x00, 0x77));
+				Label & nameLabel = userCell.addLabel(name);
+				nameLabel.setPosition((cellWidth - nameLabel.getWidth())/2, 10);
+				nameLabel.setColor(BUTTON_TEXT_COLOR);
+			}
+		}
+		
+	}
 	Button<GraphicChampionshipManager> & backButton = _canvas.addButton<GraphicChampionshipManager>(
 		&GraphicChampionshipManager::updateChampionships, this, "Back");
 	backButton.setPosition(window().getSize().x-backButton.getWidth()-MARGIN, window().getSize().y-backButton.getHeight()-MARGIN);
