491507ab7af73d10c1c1fe1b57568211b4e2150c
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-03-14 17:26:00 +0000

    [add] Put/Swap players to/from Squad in CLI
    Change-Id: Ie255867c512f5e506211de9e85bfe60096ae3d93

--- client/team/CLITeamManager.cpp
+++ client/team/CLITeamManager.cpp
@@ -9,34 +9,33 @@ CLITeamManager::CLITeamManager(ClientManager const & parent) : TeamManager(paren
 void CLITeamManager::run()
 {
 	Menu _menu;
-	_menu.addToDisplay("	- Show list of players");
-	_menu.addToDisplay("    - quit to management menu\n");
+	_menu.addToDisplay("	- Put player at the selected position");
+	_menu.addToDisplay("	- Swap player at the selected position with another player");
+	_menu.addToDisplay("	- Quit to management menu\n");
 	int option;
 	_pending = 0;
-	loadPlayers();
-	_pending++;
 	do
 	{
-		showPlayers();
-		showSquad();
 		do {
 			minisleep(0.1);
 			readMessages();
 		}
 		while (_pending > 0);
+		showPlayers();
+		showSquad();
 		option = _menu.run();
 		switch(option)
 		{
 			case 1:
-				showSelectPosition();
+				showSelectPlayer();
 				break;
 			case 2:
-				showSelectPlayer();
+				showSwapPlayer();
 			default:
 				break;
 		}
 	}
-	while (option != 2);
+	while (option != 3);
 }
 
 void CLITeamManager::showPlayers(){
@@ -50,31 +49,54 @@ void CLITeamManager::showPlayers(){
 void CLITeamManager::showSquad()
 {
 	cout << "================ YOUR SQUAD ==================" << endl;
-	cout << LEFT_CHASER << " - \e[34mLeft Chaser\e[0m : 		" << user().squad.players[LEFT_CHASER]->getName() << endl;
-	cout << CENTER_CHASER << " - \e[34mCenter Chaser\e[0m : 		" << user().squad.players[CENTER_CHASER]->getName() << endl;
-	cout << RIGHT_CHASER << " - \e[34mRight Chaser\e[0m : 		" << user().squad.players[RIGHT_CHASER]->getName() << endl;
-	cout << LEFT_BEATER << " - \e[34mLeft Beater\e[0m : 		" << user().squad.players[LEFT_BEATER]->getName() << endl;
-	cout << RIGHT_BEATER << " - \e[34mRight Beater\e[0m : 		" << user().squad.players[RIGHT_BEATER]->getName() << endl;
-	cout << SEEKER << " - \e[34mSeeker\e[0m : 			" << user().squad.players[SEEKER]->getName() << endl;
-	cout << KEEPER << " - \e[34mKeeper\e[0m : 			" << user().squad.players[KEEPER]->getName() << endl;
-	if (getSelectedPosition() > -1)
-		cout << "Current selected position is " << getSelectedPosition() << endl;
+	cout << LEFT_CHASER << " - \e[34mLeft Chaser\e[0m : 		" << user().squad.players[LEFT_CHASER]->getName() << " - ID: " << user().squad.players[LEFT_CHASER]->getMemberID() << endl;
+	cout << CENTER_CHASER << " - \e[34mCenter Chaser\e[0m : 		" << user().squad.players[CENTER_CHASER]->getName() << " - ID: " << user().squad.players[CENTER_CHASER]->getMemberID() << endl;
+	cout << RIGHT_CHASER << " - \e[34mRight Chaser\e[0m : 		" << user().squad.players[RIGHT_CHASER]->getName() << " - ID: " << user().squad.players[RIGHT_CHASER]->getMemberID() << endl;
+	cout << LEFT_BEATER << " - \e[34mLeft Beater\e[0m : 		" << user().squad.players[LEFT_BEATER]->getName() << " - ID: " << user().squad.players[LEFT_BEATER]->getMemberID() << endl;
+	cout << RIGHT_BEATER << " - \e[34mRight Beater\e[0m : 		" << user().squad.players[RIGHT_BEATER]->getName() << " - ID: " << user().squad.players[RIGHT_BEATER]->getMemberID() << endl;
+	cout << SEEKER << " - \e[34mSeeker\e[0m : 			" << user().squad.players[SEEKER]->getName() << " - ID: " << user().squad.players[SEEKER]->getMemberID() << endl;
+	cout << KEEPER << " - \e[34mKeeper\e[0m : 			" << user().squad.players[KEEPER]->getName() << " - ID: " << user().squad.players[KEEPER]->getMemberID() << endl;
 	cout << "==============================================" << endl;
 }
 
 void CLITeamManager::showSelectPosition()
 {
+	int position;
+	do
+	{
+		cout << "Choose a position (number)" << endl << ">";
+		cin >> position;
+	}
+	while (position < 0 || position > 6);
+	selectPosition(position);
 
 }
 
 void CLITeamManager::showSelectPlayer()
 {
-
+	int player_id;
+	do
+	{
+		cout << "Choose the player (ID) you want to add in your squad (-1 to quit)" << endl << ">";
+		cin >> player_id;
+		if (player_id == -1) return;
+	}
+	while(!isPlayerInTeam(player_id) || isPlayerInSquad(player_id));
+	showSelectPosition();
+	putPlayerAtPosition(player_id, getSelectedPosition());
 }
 
-void CLITeamManager::onPlayersLoad()
+void CLITeamManager::showSwapPlayer()
 {
-	_pending--;
+	int player_id;
+	do
+	{
+		cout << "Choose the player (ID) you want to swap" << endl << ">";
+		cin >> player_id;
+	}
+	while(!isPlayerInSquad(player_id));
+	showSelectPosition();
+	swapPlayers(player_id, getSelectedPosition());
 }
 
 void CLITeamManager::onMatchStart(){
--- client/team/CLITeamManager.hpp
+++ client/team/CLITeamManager.hpp
@@ -14,8 +14,28 @@ public:
 	virtual void showSquad();
 	virtual void showSelectPosition();
 	virtual void showSelectPlayer();
-	virtual void onPlayersLoad();
+	virtual void showSwapPlayer();
 	virtual void onMatchStart();
+
+	bool isPlayerInTeam(int player_id)
+	{
+		for (size_t i = 0; i < user().players.size(); ++i)
+		{
+			if (user().players[i].getMemberID() == player_id)
+				return true;
+		}
+		return false;
+	}
+
+	bool isPlayerInSquad(int player_id)
+	{
+		for (size_t i = 0; i < 7; ++i)
+		{
+			if (user().squad.players[i]->getMemberID() == player_id)
+				return true;
+		}
+		return false;
+	}
 };
 
 #endif // __CLI_TEAM_MANAGER_HPP
