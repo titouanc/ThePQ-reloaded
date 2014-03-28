d48203cdd2169e484932f77a4e9864d59386d046
Titouan Christophe <tichrist@ulb.ac.be>
2014-03-28 11:52:02 +0100

    [add] Notifications sounds

--- client/GUINotifier.hpp
+++ client/GUINotifier.hpp
@@ -3,15 +3,16 @@
 
 #include <match/GraphicMatchManager.hpp>
 #include <graphics/MessageBox.hpp>
+#include "graphics/Bank.hpp"
 
 /*! Graphical module mixin that allows automatic handling of notifications and
     incoming messages in main event loop */
 #define NOTIFIABLE \
 virtual void loop(){readMessages();handleNotification();}\
 virtual void launchMatch(){GraphicMatchManager match(*this, _controller);match.run();redrawCanvas();}\
-virtual void onOtherAccept(std::string const & user){launchMatch();}\
+virtual void onOtherAccept(std::string const & user){sf::Sound sound(SOUNDS.get("invite_accept.wav")); sound.play(); launchMatch();}\
 virtual void onOtherDeny(std::string const & user){displayError(user + " doesn't want to play with you");}\
-virtual void onInvite(std::string const & otherUser){std::string question = otherUser + " has invited you to play a friendly game.\nAccept ?";if (confirm(question)){acceptInvitationFromUser(otherUser);launchMatch();} else {denyInvitationFromUser(otherUser);}}\
+virtual void onInvite(std::string const & otherUser){sf::Sound sound(SOUNDS.get("invite.wav")); sound.play(); std::string question = otherUser + " has invited you to play a friendly game.\nAccept ?";if (confirm(question)){acceptInvitationFromUser(otherUser);launchMatch();} else {denyInvitationFromUser(otherUser);}}\
 virtual void onMessage(std::string const & message){displayOk(message);}\
 virtual void onNotificationResponse(bool success, std::string const & response, std::string const & message){(success) ? displayOk(message) : displayError(message);}\
 virtual void onMatchPending(){MessageBox m(_controller, "Next championship match is close. What do you want to do ?", {"Ready to play !", "Withdraw"});int res = m.showBox();(res==0) ? readyForMatch() : withdrawFromMatch();}\
