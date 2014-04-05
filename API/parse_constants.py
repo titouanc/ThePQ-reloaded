c7644a03a4472972d80073a500c78746432623b9
Titouan Christophe <tichrist@ulb.ac.be>
2014-04-05 13:11:05 +0200

    [enh] Parse numeric constants too

--- API/parse_constants.py
+++ API/parse_constants.py
@@ -1,6 +1,10 @@
 import re
 
-PATTERN = re.compile(r'static +const +std::string +(\w[\w\d_]+) *= *"([^"]+)"')
+PATTERNS = {
+	str : re.compile(r'static +const +std::string +(?P<name>\w[\w\d_]*) *= *"(?P<val>[^"]+)"'),
+	int : re.compile(r'static +const(?: +unsigned)? +int +(?P<name>\w[\w\d_]*) *= *(?P<val>\d+)'),
+	float : re.compile(r'static +const +(double|float) +(?P<name>\w[\w\d_]*) *= *(?P<val>[\d\.]+)')
+}
 
 def parse_constants(filename):
 	"""
@@ -10,9 +14,11 @@ def parse_constants(filename):
 	res = {}
 	with open(filename) as f:
 		for line in f:
-			match = PATTERN.search(line)
-			if match:
-				res[match.group(1)] = match.group(2)
+			for typ in PATTERNS:
+				regexp = PATTERNS[typ]
+				match = regexp.search(line)
+				if match:
+					res[match.group('name')] = typ(match.group('val'))
 	return res
 
 K = parse_constants("../common/Constants.hpp")
